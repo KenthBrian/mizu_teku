3df628ec60486d729da870a61923624fe3b851c3
KenthBrian <kenthbrian88@gmail.com>
2025-10-22 15:55:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1504,6 +1504,7 @@ const firebaseConfig = {
       middleinitialInput.style.display = "block";
       confirmPasswordInput.style.display = "block";
       emailInput.style.display = "block";
+      rememberInput.style.display = "none";
     } else {
       modalTitle.textContent = "Student Sign In";
       modalDesc.textContent = "Welcome back! Please enter your name and password:";
@@ -1513,6 +1514,7 @@ const firebaseConfig = {
       lastnameInput.style.display = "none";
       middleinitialInput.style.display = "none";
       confirmPasswordInput.style.display = "none";
+      rememberInput.style.display = "block";
     }
 
     lastnameInput.value = "";
